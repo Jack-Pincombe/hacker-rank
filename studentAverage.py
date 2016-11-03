34a6ac53b21c29efbbd5d90be7a63eb0c6d1f1f2
Jack Pincombe <jackpincombe1@gmail.com>
2016-11-03 16:57:33 +0000

    added comments to average file

--- studentAverage.py
+++ studentAverage.py
@@ -1,15 +1,16 @@
 import sys
-
+#two arrays for the student and their scores.
 studentList = ['Jack','Joe','Harry']
 marks = [88, 56, 80]
 
 
 
-
+#iterating though the students 
 def studentAverage(studentList,marks):
     for i in range(len(studentList)):
         print(studentList[i], end=" ")
         mark = marks[i]
+        #comparing student score to class average
         if mark >= classAverage(marks):
             print("is to be congratulated")
         else:
@@ -18,7 +19,7 @@ def studentAverage(studentList,marks):
     pass
 
 
-
+#simple function to obtain the average of an int array
 def classAverage(y):
     total = 0
     for i in marks:
